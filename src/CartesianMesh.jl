3d574c6a160be892e6934684621b233c7cf709b2
Arjun Narayanan <arjun.narayanan91@gmail.com>
2020-08-07 17:39:53 -0700

    Added export for mesh attributes

--- src/CartesianMesh.jl
+++ src/CartesianMesh.jl
@@ -5,6 +5,6 @@ using StaticArrays
 include("uniform_mesh.jl")
 include("mesh_attributes.jl")
 
-export UniformMesh
+export UniformMesh, faces_per_cell, neighbors, element, number_of_elements
 
 end # module
